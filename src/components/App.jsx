d1a4624367d9f74f4ba23952d3ef4986b81cd161
Anatolii Tokariev <anatoliytockarev2709@gmail.com>
2023-08-10 21:39:24 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
+import { useState } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+import { Container } from './App.styled';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleButtonClick = ({ target }) => {
+    target.name === 'good'
+      ? setGood(prevState => prevState + 1)
+      : target.name === 'neutral'
+      ? setNeutral(prevState => prevState + 1)
+      : setBad(prevState => prevState + 1);
+  };
+
+  const countTotalFeedback = () => {
+    let total = good + neutral + bad;
+    return total;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    let total = good + neutral + bad;
+
+    if (good === 0) {
+      return 0;
+    }
+    return Math.round((100 / total) * good);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions onLeaveFeedback={handleButtonClick} />
+      </Section>
+      {countTotalFeedback() === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      )}
+    </Container>
   );
 };
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const ButtonFeedback = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const ButtonStyle = styled.button`
+  cursor: pointer;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: #8e8e8e;
+
+  &:hover {
+    background-color: ${props =>
+      props.name === 'good'
+        ? '#21C500'
+        : props.name === 'neutral'
+        ? '#F3FF00'
+        : props.name === 'bad'
+        ? '#FF0C0C'
+        : '#8E8E8E'};
+    color: ${props => (props.name === 'neutral' ? 'black' : 'white')};
+  }
+`;
